6fb2842821bae2c90cee660f4f0cf3edd43d6f19
JakubSkiba15 <95942216+JakubSkiba15@users.noreply.github.com>
2021-12-10 22:58:14 +0100

    zad 5

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+	int n, suma;
+	cout << "Podaj n: ";
+	cin >> n;
+	suma = 0;
+
+	for (int i = 2; i <= n; i += 2)
+	{
+		suma += i;
+	}
+
+	cout << "Suma parzystych elementow w ciagu " << n << ">0 jest rowna " << suma;
+
+
+	return 0;
+}
